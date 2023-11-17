319f557e7b4750a7a8010ef564c57acc4c3058f7
Aasamvikasreddy <148895898+Aasamvikasreddy@users.noreply.github.com>
2023-11-17 11:40:37 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,108 @@
+function showIntroduction() {
+    document.getElementById('content').innerHTML = `
+        <h2>Introduction</h2>
+        <p>Welcome to our website dedicated to raising awareness about waterborne diseases and promoting access to clean water.
+		• The Protocol on Water and Health and requirements relating to water 
+related disease surveillance and outbreak management
+• International Health Regulations (IHR) core requirements:
+• Definition of water related infectious disease (WRID)
+• Pathogens transmitted through drinking-water
+•drinking-water systems as a source of WRID
+• Burden of WRID in the European Region
+• The need to strengthen WRID surveillance and outbreak management 
+capacit
+•Article 8: 
+• Establish and maintain surveillance and early warning 
+systems
+• Develop national and local contingency plans for 
+responding to outbreaks, incidents and risks
+• Strengthen response capacity
+•Article 6.2: 
+• Establish and publish targets to reduce WRD outbreaks 
+and incidents
+•Article 13: 
+• Strengthen transboundary cooperation on early-warning 
+and response systems</p>
+<img src="https://www.healthcarebusinesstoday.com/wp-content/uploads/2021/06/BackflowDirect-96125-Water-Supply-Clean-image1.jpg" alt="Introduction Image">
+        <!-- Add more content as needed -->
+		<p>Drink only bottled, boiled, or chemically treated water and bottled or canned  beverages. When using bottled drinks, make sure the seal has not been broken. Carbonated water may be safer than non-carbonated water. Avoid tap water, fountain drinks, and ice cubes.
+To disinfect your own water, choose one of the following options:
+Boil it for 1 minute, or
+Filter it and add either ½ an iodine tablet or 2 drops of household bleach per liter/quart of water, or
+Use commercial water chlorination tablets according to the manufacturer’s instructions.
+Wash your hands often with soap and clean water, especially before you eat or prepare food and after using the bathroom.
+If no water and soap are available, use an alcohol-based hand sanitizer with at least 60% alcohol.
+Use bottled, boiled, or chemically treated water to wash dishes, brush teeth, wash and prepare food, and make ice.
+Eat foods that are packaged or that are freshly cooked and served hot.
+Do not eat raw or undercooked meats and seafood, or raw or undercooked fruits and vegetables unless they are peeled.
+Dispose of feces in a sanitary manner to prevent contamination of water and food sources.</p>
+    `;
+}
+
+function showDiseases() {
+    document.getElementById('content').innerHTML = `
+        <h2>Waterborne Diseases</h2>
+		
+        <ul>
+            <li>Cholera</li>
+			<p>The cholera bacterium is usually found in water or in foods that have been contaminated by feces (poop) from a person infected with cholera bacteria. Cholera is most likely to occur and spread in places with inadequate water treatment, poor sanitation, and inadequate hygiene.
+
+Cholera bacteria can also live in the environment in brackish rivers and coastal waters. Shellfish eaten raw have been a source of infection. Rarely, people in the U.S. have contracted cholera after eating raw or undercooked shellfish from the Gulf of Mexico.</p>
+            <li>Dysentery</li>
+			<p>Bacillary dysentery is the most common type of dysentery. It results from bacteria called Shigella. The disease is called shigellosis. About 500,000 people in the U.S. get it every year.
+
+Amoebic dysentery comes from a parasite called Entamoeba histolytica. You're more likely to get this kind of dysentery if you travel to a tropical location that doesn’t have good sanitation.
+
+You can get dysentery if you eat food that's been prepared by someone who has it. For example, you might get it if the person who made your food is sick and didn't properly wash their hands. Or you can get dysentery if you touch something that has the parasite or bacteria on it, such as a toilet handle or sink knob. </p>
+            <li>Giardiasis</li>
+			<p>Symptoms
+Some people with giardia infection never develop signs or symptoms, but they still carry the parasite and can spread it to others through their stool. For those who do get sick, signs and symptoms usually appear one to three weeks after exposure and may include:
+
+Watery, sometimes foul-smelling diarrhea that may alternate with soft, greasy stools
+Fatigue
+Stomach cramps and bloating
+Gas
+Nausea
+Weight loss
+Signs and symptoms of giardia infection may last two to six weeks, but in some people they last longer or recur.</p>
+            <!-- Add more diseases as needed -->
+        </ul>
+		<img src="https://th.bing.com/th/id/OIP.RsOaJ-ARZCqYjYMxVNDU-QHaE6?w=1113&h=739&rs=1&pid=ImgDetMain" alt="Diseases Image">
+        <!-- Add more content as needed -->
+		<p> A single-dose live oral cholera vaccine, called Vaxchora (lyophilized CVD 103-HgR), is approved by the U.S. Food and Drug Administration for people 2–64 years old who are traveling to an area of active cholera transmission. Three other oral cholera vaccines are approved by the World Health Organization (WHO) – Dukoral, ShanChol, and Euvichol-Plus/Euvichol. These vaccines are not available in the United States, but may be available at your travel destination or if you’re living outside the United States. Check CDC’s Travel Health Notices website to identify areas with active cholera transmission. Visit a doctor or travel clinic to talk about vaccination if you will be traveling to or living in one of these areas.
+Visit CDC’s global immunization website to learn more about CDC’s global work with cholera vaccines.</p>
+    `;
+}
+
+function showSolutions() {
+    document.getElementById('content').innerHTML = `
+        <h2>Clean Water Solutions</h2>
+        <p>Access to clean water is crucial to prevent waterborne diseases. Here are some solutions:
+		Right now, about 10 percent of the world lacks basic access to safe drinking water. Families drink from surface water sources like ponds and rivers or walk hours a day to reach a water well.
+
+Every community is unique, and there is no one-size-fits-all clean water solution. One sustainable way to solve the global water crisis is to first identify the most appropriate water source. Factors like the following should be considered when identifying the best water solution:
+
+Are there existing water sources, like a natural spring, that can be protected from contamination? Protecting an existing source can be a cost-effective, preferred solution for communities already accustomed to using that source.
+What cultural water preferences are at play? Some communities might prefer the taste of spring water over groundwater. Can a spring be protected or a water filtration system put in place to meet the cultural needs?
+How cost-effective are the various clean water solutions? For a sustainable change, communities must be able to afford any upfront costs as well as the costs of any replacement parts and maintenance of their water source.
+Are there government regulations that may prevent construction of the preferred water solution or slow down the process?</p>
+        <ul>
+            <li>Water purification systems</li>
+            <li>Community education on water hygiene</li>
+            <li>Infrastructure development for clean water supply</li>
+            <!-- Add more solutions as needed -->
+        </ul>
+		<img src="https://i.ytimg.com/vi/sfrlJOEtnqI/maxresdefault.jpg" alt="Solutions Image">
+		<p>It is estimated that waterborne diseases have an economic burden of approximately USD 600 million a year in India. This is especially true for drought- and flood-prone areas, which affected a third of India’s population in the past couple of years. 
+
+Moreover, two-thirds of India’s 718 districts are affected by extreme water depletion. One of the challenges is the fast rate of groundwater depletion in India, which is known as the world’s highest user of this source due to the proliferation of drilling over the past few decades. Groundwater from over 30 million access points supplies 85 per cent of drinking water in rural areas and 48 per cent of water requirements in urban areas.  (Source: JMP 2017)         
+
+All children have the right to clean water and basic sanitation, as stated in the Convention on the Rights of a Child. The ultimate aim of UNICEF’s work in water, sanitation and hygiene (WASH) is to ensure that all children fulfill this right, and that no child is left behind.       
+
+When families do not have a safe and reliable water source, preferably direct to their home, then it is often women and children that are responsible for collecting water. School attendance in India decreases when children are required to spend hours collecting water. A 22 per cent increase in school dropout rates has been reported in drought-affected states</p>
+        <!-- Add more content as needed -->
+    `;
+}
+
+// Show introduction by default
+showIntroduction();
